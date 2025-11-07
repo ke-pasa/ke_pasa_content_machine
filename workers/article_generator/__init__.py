ce2736cd983e6b0c1e6fcb106907ec0702879804
Pavel Zimahorau <pavel.zimogorov@gmail.com>
2025-11-07 18:49:35 +0100

    second attempt

--- workers/article_generator/__init__.py
+++ workers/article_generator/__init__.py
@@ -2,7 +2,7 @@
 Article Generator Worker - module for generating articles
 """
 
-from .worker import ArticleGeneratorWorker
+from .ArticleGenerator import ArticleGenerator as ArticleGeneratorWorker
 from .config import GeneratorConfig
 
 __all__ = ['ArticleGeneratorWorker', 'GeneratorConfig']
